05a3b1399e3bdfc5929e1f7734111e8b12402689
Ramadhan Agies Ananda Wahyudi <agieswahyudi@gmail.com>
2024-09-16 12:28:55 +0700

    update deploy

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,34 +15,46 @@ class UserSeeder extends Seeder
     {
         $users = [
             [
+                'name' => 'Kabag Sarpras',
+                'email' => 'kabag.sarpras@alhasra.sch.id',
+                'role' => 'admin',
+                'password' => Hash::make('zaHh8LWK)ru4neTMd7>E;x')
+            ],
+            [
                 'name' => 'Admin Sarpras',
                 'email' => 'admin.sarpras@alhasra.sch.id',
                 'role' => 'admin',
-                'password' => Hash::make('sarpras49')
+                'password' => Hash::make('Fh/^%;A@Ec3{_Zj$2!m=+p')
+            ],
+            [
+                'name' => 'IT Support Sarpras',
+                'email' => 'it.sarpras@alhasra.sch.id',
+                'role' => 'admin',
+                'password' => Hash::make('Jk?T6)(>UACyK*x2j.3s^!')
             ],
             [
                 'name' => 'Yayasan Al-hasra',
                 'email' => 'yayasan.sarpras@alhasra.sch.id',
                 'role' => 'user',
-                'password' => Hash::make('sarprasyayasan77')
+                'password' => Hash::make('RQ!cE-ua#~gV^A_SNLq7Cn')
             ],
             [
                 'name' => 'SMA Al-hasra',
                 'email' => 'sma.sarpras@alhasra.sch.id',
                 'role' => 'user',
-                'password' => Hash::make('sarprassma32')
+                'password' => Hash::make('R[Mh+35vU$,fGdwZXKyA-4')
             ],
             [
                 'name' => 'SMP Al-hasra',
                 'email' => 'smp.sarpras@alhasra.sch.id',
                 'role' => 'user',
-                'password' => Hash::make('sarprassmp78')
+                'password' => Hash::make('VhR;(HTSaBF4eGyg.sp^]n')
             ],
             [
                 'name' => 'SMK Al-hasra',
                 'email' => 'smk.sarpras@alhasra.sch.id',
                 'role' => 'user',
-                'password' => Hash::make('sarprassmk65')
+                'password' => Hash::make('aLUT@D;*u/7)H~wSFN"M8-')
             ],
         ];
 
